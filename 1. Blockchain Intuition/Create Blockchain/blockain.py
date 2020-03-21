d9271277674a5a211b8beeb7944ca0d360219d45
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2020-03-21 16:00:34 -0600

    Create the Blockchain class
    Created teh basics block of our blockchain and made it so that when it is called in postman I can see the details of that block itself.

--- 1. Blockchain Intuition/Create Blockchain/blockain.py
+++ 1. Blockchain Intuition/Create Blockchain/blockain.py
@@ -1,2 +1,26 @@
 # Module 1 - Create a Blockchain
 
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Part 1 - Building a Blockchain
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0') # Common practice is 0
+
+    def create_block(self, proof, previous_hash):
+        block={'index' : len(self.chain) + 1,
+               'timestamp': str(datetime.datetime.now()),
+               'proof': proof,
+               'previous_hash': previous_hash}
+        self.chain.append(block)
+        return block
+    
+
+# Part 2 - Mining our blockchain
+
+
